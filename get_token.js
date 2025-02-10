b68ae78249167ae27b766baa6520362eff75b2e0
euxhhc <y0775381337@gmail.com>
2025-02-10 22:13:02 +0200

    Update get_token.js

--- get_token.js
+++ get_token.js
@@ -13,5 +13,8 @@ export default async function handler(req, res) {
         }
     } catch (error) {
         res.status(500).json({ error: "שגיאה בשרת" });
+        module.exports = (req, res) => {
+    res.json({ message: "Token API is working!" });
+};
     }
 }
